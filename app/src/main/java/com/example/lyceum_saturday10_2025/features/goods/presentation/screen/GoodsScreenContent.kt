79191ee648ee9a6b4cc93c951805ce8c2f722c5e
mariavyzhletsova-gif <mariavyzhletsova@gmail.com>
2025-12-23 21:32:08 +0300

    Update GoodsScreenContent.kt

--- app/src/main/java/com/example/lyceum_saturday10_2025/features/goods/presentation/screen/GoodsScreenContent.kt
+++ app/src/main/java/com/example/lyceum_saturday10_2025/features/goods/presentation/screen/GoodsScreenContent.kt
@@ -25,8 +25,9 @@ import com.example.lyceum_saturday10_2025.features.goods.presentation.model.Good
 @Composable
 fun GoodsScreenContent(
     state: GoodsUiState,
-    onAddClicked: (String, String) -> Unit,
+    onAddClicked: (String, String, String) -> Unit,
     onGoodClicked: (GoodsItem) -> Unit,
+    onDeleteGood: (Long) -> Unit,
 ) {
     Column {
         var nameTextFieldValue by remember { mutableStateOf("") }
@@ -55,8 +56,24 @@ fun GoodsScreenContent(
 
         Spacer(Modifier.height(16.dp))
 
+        var imageUrlTextFieldValue by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = imageUrlTextFieldValue,
+            onValueChange = { newValue ->
+                imageUrlTextFieldValue = newValue
+            },
+            placeholder = {
+                Text("URL картинки")
+            }
+        )
+
+        Spacer(Modifier.height(16.dp))
+
         Button(onClick = {
-            onAddClicked(nameTextFieldValue, descriptionTextFieldValue)
+            onAddClicked(nameTextFieldValue, descriptionTextFieldValue, imageUrlTextFieldValue)
+            nameTextFieldValue = ""
+            descriptionTextFieldValue = ""
+            imageUrlTextFieldValue = ""
         }) {
             Text("Добавить товар")
         }
@@ -66,7 +83,15 @@ fun GoodsScreenContent(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(state.items) { item ->
-                GoodsCard(item, onGoodClicked)
+                GoodsCard(
+                    item, 
+                    onGoodClicked,
+                    onDeleteClicked = { 
+                        if (item.id != null) {
+                            onDeleteGood(item.id)
+                        }
+                    }
+                )
             }
         }
     }
@@ -75,5 +100,5 @@ fun GoodsScreenContent(
 @Composable
 @Preview
 private fun GoodsScreenPreview() {
-    GoodsScreenContent(GoodsUiState(), { _, _ -> }, { _ -> })
+    GoodsScreenContent(GoodsUiState(), { _, _, _ -> }, { _ -> }, { _ -> })
 }
